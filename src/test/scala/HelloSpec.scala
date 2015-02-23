ac041c45e7794c6af8a8689d2dd9ccdaa77af8da
ab <adamdd55@gmail.com>
2015-02-24 00:23:08 +0100

    fixed deprecations in scalatest: ShouldMatchers are depreciated, should be === is depreciated

--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -1,8 +1,7 @@
 import org.scalatest._
-import org.scalatest.matchers.ShouldMatchers
 
-class HelloSpec extends FlatSpec with ShouldMatchers {
+class HelloSpec extends FlatSpec with Matchers {
   "Hello" should "have tests" in {
-    true should be === true
+    true should === (true)
   }
 }
